99722820f913baa60043f1b5ba5b9e8b60ea398a
EGAAJITH-G <egaajith4343@gmail.com>
2026-06-12 00:52:34 +0530

    shoppageoverjun12

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-import React from 'react';
-import About from './pages/about/About';
-=======
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Contact from './pages/Contact';
 import Footer from './components/Footer';
->>>>>>> b37389720d1a9a565b8eb5dc44628bcd8175e2c0
+import About from './pages/about/About';
+import Shop from './pages/shop/Shop';
+import ProductDetails from './pages/shop/ProductDetails';
 import './App.css';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <About />
-    </div>
-=======
     <Router>
       <div className="App">
         <Navbar />
@@ -23,8 +16,9 @@ function App() {
         <main style={{ marginTop: '160px', padding: '20px', textAlign: 'center' }}>
           <Routes>
             <Route path="/" element={<h1>Welcome to Dazzling Sky</h1>} />
-            <Route path="/about" element={<h1>About Us</h1>} />
-            <Route path="/shop" element={<h1>Shop Collection</h1>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/contacts" element={<Contact />} />
             <Route path="/blog" element={<h1>Our Blog</h1>} />
             <Route path="/faq" element={<h1>FAQ</h1>} />
@@ -33,7 +27,6 @@ function App() {
         <Footer />
       </div>
     </Router>
->>>>>>> b37389720d1a9a565b8eb5dc44628bcd8175e2c0
   );
 }
 
